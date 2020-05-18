3969e95bb956624be679963057dc7a8c8410e6a9
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-05-18 14:33:09 +0700

    disable console.log

--- js/controller.js
+++ js/controller.js
@@ -162,11 +162,12 @@
       items = data;
     });
 
-    items.forEach(function (item) {
-      if (item.id === id) {
-        console.log('Element with ID: ' + id + ' has been removed.');
-      }
-    });
+    // useless loop
+    // items.forEach(function (item) {
+    //   if (item.id === id) {
+    //     console.log('Element with ID: ' + id + ' has been removed.');
+    //   }
+    // });
 
     self.model.remove(id, function () {
       self.view.render('removeItem', id);
